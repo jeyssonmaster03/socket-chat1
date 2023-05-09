f41f43a8f30b4599b83d8b80496f204c6972d7c4
Jeysson Ferrera <branley0330@gmail.com>
2023-05-09 11:24:17 -0400

    segundo commit

---
+++ server/utilidades/utilidades.js
@@ -0,0 +1,23 @@
+
+
+const crearMensaje=(nombre, mensaje)=>{
+
+    return {
+        nombre,
+        mensaje,
+        fecha: new Date().getTime()
+    }
+
+
+}
+
+
+
+module.exports={
+    crearMensaje
+}
+
+
+
+
+
